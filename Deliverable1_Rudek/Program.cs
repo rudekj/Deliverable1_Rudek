8e9466b2f26de127773730f467a6ac91b1e33944
rudek <rudek@DESKTOP-OOLQ00I>
2020-01-23 22:23:32 -0500

    Add project files.

---
+++ Deliverable1_Rudek/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Deliverable1_Rudek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to continue");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
